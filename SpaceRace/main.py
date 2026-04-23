0b9cebc4c185fe1a2c44a640399f4848a95da4be
Henry Slough <henry.w.slough@gmail.com>
2026-04-23 09:11:40 -0400

    Added camera position mean functionality for equal space race movement

--- SpaceRace/main.py
+++ SpaceRace/main.py
@@ -14,34 +14,46 @@ screen.set_fill_color((200, 200, 200))
 screen.add_layer("sprites")
 
 
-player_distance = 50
+player_distance = 128
 for p in range(config.TOTAL_PLAYERS):
     
     player = Player.Player(32, 32, screen.layers["sprites"])
     player.id = len(screen.layers["sprites"])-1
     player.image.fill((100, 100, 100))
+
     player.set_position(player.id * player_distance, 0)
-
-
-center = GameObject.GameObject(1, 1, screen.layers["sprites"])
-
-
-camera = Camera.Camera(center)
 
 
 total_x = 0
 for g in screen.layers.values():
     for s in g.sprites():
         total_x += s.rect.x
-center.set_position(total_x//len(screen.layers), 0)
+
+
+center = GameObject.GameObject(16, 16, screen.layers["sprites"])
+center.image.fill((255, 255, 255, 150))
+
+camera = Camera.Camera(center)
+
+center.set_position(total_x//len(screen.layers["sprites"].sprites())+(player_distance//2-16), 0)
+
+dist_y = 0
 
 running = True
 while running:
 
+
+    total_y = 0
+    for g in screen.layers.values():
+        for s in g:
+            total_y += s.rect.y
+
+            if s.rect.y < 0:
+                camera.set_zoom(0.5)
+
+    center.set_position(0, total_y//len(screen.layers["sprites"].sprites())+(player_distance//2-16))
+
    
-    
-
-
     if screen.has_quit():
         running = False
 
